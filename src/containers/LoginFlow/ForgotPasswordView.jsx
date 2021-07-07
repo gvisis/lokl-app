d3a61e4b309a06c903b17d5aa6be4ad5263f2f59
Gvidas <gvisis@gmail.com>
2021-07-07 03:17:20 +0300

    reset password needs testing after login

--- src/containers/LoginFlow/ForgotPasswordView.jsx
+++ src/containers/LoginFlow/ForgotPasswordView.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import auth from '@react-native-firebase/auth';
+
 import {StyleSheet, Text} from 'react-native';
 import {AuthContainer} from './';
 import {CustomBtn, CustomInput} from '../../components';
 import {ROUTES} from '../../routes/RouteNames';
 
 export const ForgotPasswordView = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const handleResetPassword = usersEmail => {
+    auth()
+      .sendPasswordResetEmail(usersEmail)
+      .then(() => {
+        console.warn(`Password reset email sent to ${usersEmail}`);
+      })
+      .catch(error => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+      });
+    setEmail('');
+  };
   return (
     <AuthContainer headerTitle="Password reset">
-      <CustomInput placeholder="Enter your email" />
-      <CustomBtn text="Reset" center activeOpacity={0.8} />
-      <CustomBtn text="Go back" center width="30" backgroundColor="red" activeOpacity={0.8} onPress={() => navigation.goBack()}/>
+      <CustomInput
+        placeholder="Enter your email"
+        onChangeText={setEmail}
+        value={email}
+      />
+      <CustomBtn
+        text="Reset"
+        center
+        activeOpacity={0.8}
+        onPress={() => handleResetPassword(email)}
+      />
+      <CustomBtn
+        text="Go back"
+        center
+        width="30"
+        backgroundColor="red"
+        activeOpacity={0.8}
+        onPress={() => navigation.goBack()}
+      />
     </AuthContainer>
   );
 };
