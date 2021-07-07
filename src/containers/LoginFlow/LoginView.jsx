17603581a0c90218a31c95b987c5eb6c6195c62d
Gvidas <gvisis@gmail.com>
2021-07-07 04:38:59 +0300

    login works

--- src/containers/LoginFlow/LoginView.jsx
+++ src/containers/LoginFlow/LoginView.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import React, {useState} from 'react';
+
+import {useGlobalContext} from '../../state/context';
+import {Text} from 'react-native';
 import {AuthContainer} from './';
 import {CustomBtn, CustomInput} from '../../components';
 import {ROUTES} from '../../routes/RouteNames';
 
 export const LoginView = ({navigation}) => {
+  const {isLoggedIn} = useGlobalContext();
+  console.warn(isLoggedIn);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const {handleLogin, handleRegister, handleLogout} = useGlobalContext();
   return (
     <AuthContainer headerTitle="Login">
-      <CustomInput placeholder="Email" />
-      <CustomInput placeholder="Password" />
-      <CustomBtn text="Login" center activeOpacity={0.8} />
+      <CustomInput placeholder="Email" onChangeText={setEmail} value={email} />
+      <CustomInput
+        placeholder="Password"
+        onChangeText={setPassword}
+        value={password}
+      />
+      <CustomBtn
+        text="Login"
+        center
+        activeOpacity={0.8}
+        onPress={() => handleLogin(email, password)}
+      />
       <CustomBtn
         text="Forgotten password?"
         center
         activeOpacity={0.8}
-        width='50'
+        width="50"
         fontSize="20"
         textTransform="uppercase"
         onPress={() => navigation.navigate(ROUTES.ForgotPassword)}
-
       />
       <Text style={{color: 'white', marginTop: 5, lineHeight: 17}}>or</Text>
       <CustomBtn
--- src/state/app/AppConstants.js
+++ src/state/app/AppConstants.js
@@ -1,3 +1,5 @@
 export const appConstants = {
   IS_LOGGED_IN: 'app/IS_LOGGED_IN',
+  LOGIN: 'app/LOGIN',
+  LOGOUT: 'app/LOGOUT',
 };
