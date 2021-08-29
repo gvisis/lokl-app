b5b1498d4b5f7c2b6eb5704b9bb6dfc7e9283edb
Gvidas <gvisis@gmail.com>
2021-08-30 01:45:59 +0300

    fix: purchase fix

--- src/routes/CartNavigation.tsx
+++ src/routes/CartNavigation.tsx
@@ -2,21 +2,24 @@ import React, { useEffect } from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { useRoute } from '@react-navigation/core';
+import { useTheme } from '@react-navigation/native';
 
 import { ROUTES } from './RouteNames';
 import { RootStackParamList } from '../types/general';
 import { CartAddressView, CartItemsView } from '../containers/CartFlow';
-import { CartFooter, Container } from '../components';
+import { CartFooter, Container, EmptyView, ScreenLoader } from '../components';
 import { CartPaymentView } from '../containers/CartFlow/CartPaymentView';
 import { actions } from '../state/actions';
 
 const TopBar = createMaterialTopTabNavigator<RootStackParamList>();
 
 export const CartNavigation: React.FC = () => {
-  const { stage, quantity, total } = useSelector(state => state.cart);
+  const { finishPurchase, stage, quantity, total } = useSelector(
+    state => state.cart,
+  );
   const { t } = useTranslation();
 
+  const theme = useTheme();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -24,6 +27,16 @@ export const CartNavigation: React.FC = () => {
       dispatch(actions.cart.setCartStage(ROUTES.CartItemsView));
     }
   }, [stage]);
+
+  // FAKE LOADING WHEN PURCHASE IS FINISHED
+  if (finishPurchase) {
+    return (
+      <>
+        <EmptyView text={'Finishing your purchase... Thank you for waiting'} />
+        <ScreenLoader size={50} color={theme.colors.primary} />
+      </>
+    );
+  }
 
   return (
     <Container>
